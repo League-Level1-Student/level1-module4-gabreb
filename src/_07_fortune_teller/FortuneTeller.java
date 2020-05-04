1f450de73ae43cb4bff7a27d031ffcca39299fb3
gabreb <62906858+gabreb@users.noreply.github.com>
2020-05-04 13:29:37 -0700

    hi

--- src/_07_fortune_teller/FortuneTeller.java
+++ src/_07_fortune_teller/FortuneTeller.java
@@ -33,7 +33,7 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 // 2. Adjust the frameWidth and frameHeight variables to fit your image nicely (doesn’t need a new line of code)
      // 3. Complete the begin() method in the FortuneTellerRunner class
  	 // 4. add a mouse listener to the frame
-
+frame.addMouseListener(this);
     }
 
 
@@ -43,7 +43,7 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 int mouseX = e.getX();
    	 int mouseY = e.getY();
    	 // 5. Print the mouseX variable
-
+System.out.println(mouseX + " " + mouseY);
    	 // 6. Add the mouseY variable to the previous line so that it prints out too (no new line)
    	 // 7. Adjust your secret location co-ordinates here:
    	 int secretLocationX = 0;
@@ -51,7 +51,7 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 /** If the mouse co-ordinates and secret location are close, we'll let them ask a question. */
    	 if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
    		 // 8. Get the user to enter a question for the fortune teller
-
+String question = JOptionPane.showInputDialog("Ask the fortune teller a question!");
    		 // 9. Find a spooky sound and put it in your default package (freesound.org)
    		 // AudioClip sound = JApplet.newAudioClip(getClass().getResource("creepy-noise.wav"));
    		 // 10. Play the sound
--- src/_07_fortune_teller/Magic8Ball.java
+++ src/_07_fortune_teller/Magic8Ball.java
@@ -7,18 +7,33 @@ package _07_fortune_teller;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int randy = new Random().nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(randy);
 	// 4. Get the user to enter a question for the 8 ball
-
+String question2 = JOptionPane.showInputDialog("Ask the 8 ball a question!");
 	// 5. If the random number is 0
-
+if (randy==0) {
+	JOptionPane.showMessageDialog(null, "Yes");
+}
+else if (randy==1) {
+	JOptionPane.showMessageDialog(null, "No");
+}
+else if (randy==2) {
+	JOptionPane.showMessageDialog(null, "Maybe you should ask google");
+}
+else if (randy==3) {
+	JOptionPane.showMessageDialog(null, "Stop trying to hide it. We all know you have a crush on your best friend!");
+}
 	// -- tell the user "Yes"
 
 	// 6. If the random number is 1
@@ -32,5 +47,5 @@ public class Magic8Ball {
 	// 8. If the random number is 3
 
 	// -- write your own answer
-
+	}
 }
