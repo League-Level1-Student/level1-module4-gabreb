872b2c8f1721df3d4af3cf87dd8b8bd86a32e25c
gabreb <62906858+gabreb@users.noreply.github.com>
2020-05-04 13:12:32 -0700

    hi

--- src/_06_book_of_illusions/BookOfIllusions.java
+++ src/_06_book_of_illusions/BookOfIllusions.java
@@ -11,19 +11,35 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
 
 public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JLabel cat = new JLabel();
+JLabel dog = new JLabel();
+JLabel anotherImage = new JLabel();
 	public void run() {
 		// 2. make the frame visible
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		// 3. set the size of the frame
+		frame.setSize(400, 400);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		cat = loadImageFromComputer("cat.jpg");
+		dog = loadImageFromComputer("dog.jpg");
+		frame.add(panel);
+		panel.add(cat);
+		panel.add(dog);
+		frame.pack();
+		frame.addMouseListener(this);
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
 		// 7. use the "loadImage..." methods below to initialize your JLabel
 		// 8. add your JLabel to the frame
@@ -32,10 +48,16 @@ public class BookOfIllusions extends MouseAdapter {
 	}
 
 	public void mousePressed(MouseEvent e) {
+		System.out.println("clicked!");
+		panel.remove(dog);
+		panel.remove(cat);
+		anotherImage = loadImageFromComputer("monkey.jpg");
+		panel.add(anotherImage);
 		// 11. Print "clicked!" to the console when the mouse is pressed
 		// 12. remove everything from the frame that was added earlier
 		// 13. load a new image like before (this is more than one line of code)
 		// 14. pack the frame
+		frame.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
